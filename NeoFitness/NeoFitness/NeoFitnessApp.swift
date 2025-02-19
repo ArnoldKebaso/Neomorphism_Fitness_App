2f75b6f4bbe01089d1a5c60e2001402f1d96528e
ArnoldKebaso <shikukudenno@gmail.com>
2025-02-19 15:43:28 +0300

    docs:add the project files

---
+++ NeoFitness/NeoFitness/NeoFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoFitnessApp.swift
+//  NeoFitness
+//
+//  Created by Arnold Kebaso on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeoFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
