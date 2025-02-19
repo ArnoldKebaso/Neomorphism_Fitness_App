123dd6465492a5e3d3148768d8733b73c26d3ae5
ArnoldKebaso <shikukudenno@gmail.com>
2025-02-19 15:59:33 +0300

    docs: create the xcod tree structure for the app

---
+++ NeoFitness/NeoFitness/Utilities/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  NeoFitness
+//
+//  Created by Arnold Kebaso on 19/02/2025.
+//
+
+import Foundation
---
+++ NeoFitness/NeoFitness/ViewModels/WorkoutViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutViewModel.swift
+//  NeoFitness
+//
+//  Created by Arnold Kebaso on 19/02/2025.
+//
+
+import Foundation
